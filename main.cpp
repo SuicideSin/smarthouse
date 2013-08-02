12b13ae10940fc7ec8e2b695a9f5aa59fbb2cb52
Ben Neubauer <beneubauer@gmail.com>
2013-08-02 03:47:18 +0000

    Merged old RGB code with new flot code

--- main.cpp
+++ main.cpp
@@ -32,6 +32,120 @@ bool service_client(msl::socket& client,const std::string& message);
 //Global Serial Sync Object
 SerialSync ss("/dev/ttyUSB0",9600);
 
+
+
+
+
+
+
+
+
+
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+#include <stdint.h>
+#include <getopt.h>
+
+#include <sys/ioctl.h>
+
+#include <linux/types.h>
+#include <linux/spi/spidev.h>
+
+static void pabort(const char *s)
+{
+	perror(s);
+	abort();
+}
+
+static const char *device = "/dev/spidev0.0";
+static uint8_t mode;
+static uint8_t bits = 8;
+static uint32_t speed = 500000;
+static uint16_t delay;
+
+int fd_spi;
+
+#define nLEDs 18
+#define nBARs 1		// Number of WS2803 chained together
+
+uint8_t ledBar[nBARs][nLEDs];
+
+struct spi_ioc_transfer tr;
+
+
+int init_spi(){
+int ret = 0;
+
+	fd_spi = open(device, O_RDWR);
+	if (fd_spi < 0)
+		pabort("can't open device");
+
+	ret = ioctl(fd_spi, SPI_IOC_WR_MODE, &mode);
+	if (ret == -1)
+		pabort("can't set spi mode");
+
+	ret = ioctl(fd_spi, SPI_IOC_RD_MODE, &mode);
+	if (ret == -1)
+		pabort("can't get spi mode");
+
+	ret = ioctl(fd_spi, SPI_IOC_WR_BITS_PER_WORD, &bits);
+	if (ret == -1)
+		pabort("can't set bits per word");
+
+	ret = ioctl(fd_spi, SPI_IOC_RD_BITS_PER_WORD, &bits);
+	if (ret == -1)
+		pabort("can't get bits per word");
+
+	ret = ioctl(fd_spi, SPI_IOC_WR_MAX_SPEED_HZ, &speed);
+	if (ret == -1)
+		pabort("can't set max speed hz");
+
+	ret = ioctl(fd_spi, SPI_IOC_RD_MAX_SPEED_HZ, &speed);
+	if (ret == -1)
+		pabort("can't get max speed hz");
+
+
+	tr.tx_buf = (unsigned long)ledBar;
+	tr.len = nBARs * nLEDs;
+	tr.delay_usecs = delay;
+	tr.speed_hz = speed;
+	tr.bits_per_word = bits;
+
+	return ret;
+}
+
+void loadWS2803(){
+	if(ioctl(fd_spi, SPI_IOC_MESSAGE(1), &tr) < 1)
+		pabort("can't send spi message");
+}
+
+void clearWS2803(){
+int wsOut;
+
+        for(wsOut = 0; wsOut < nLEDs; wsOut++){
+		if(nBARs > 1)
+			ledBar[0][wsOut] = ledBar[1][wsOut] = 0x00;
+		else
+			ledBar[0][wsOut] = 0x00;
+		loadWS2803();
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
 //Main
 int main(int argc,char* argv[])
 {
@@ -65,6 +179,17 @@ int main(int argc,char* argv[])
 	else
 	{
 		std::cout<<"Socket "<<server_port<<" =("<<std::endl;
+		server_passed=false;
+	}
+
+	//Endable SPI
+	if(init_spi()!=-1)
+	{
+		std::cout<<"SPI =)"<<std::endl;
+	}
+	else
+	{
+		std::cout<<"SPI =("<<std::endl;
 		server_passed=false;
 	}
 
@@ -110,7 +235,7 @@ bool service_client(msl::socket& client,const std::string& message)
 	istr>>request;
 
 	//Check For a Custom Request
-	if(request=="/temperatures")
+	if(request=="/temperatures?")
 	{
 		//Package Temperatures in JSON
 		msl::json temperatures;
@@ -123,6 +248,139 @@ bool service_client(msl::socket& client,const std::string& message)
 		client<<msl::http_pack_string(temperatures.str(),"text/plain");
 
 		//Return True (We serviced the client)
+		return true;
+	}
+
+	else if(msl::starts_with(request,"/color1?"))
+	{
+		//Change ?'s to spaces
+		for(unsigned int ii=0;ii<request.size();++ii)
+			if(request[ii]=='?')
+				request[ii]=' ';
+
+		//Variables for Parsing
+		std::istringstream istr(request);
+		std::string header;
+		int red1=0;
+		int green1=0;
+		int blue1=0;
+
+		//Get Header
+		istr>>header;
+
+		//Get Color
+		istr>>red1;
+		istr>>green1;
+		istr>>blue1;
+
+		//Print Color
+		//std::cout<<"change color\t"<<red<<"\t"<<green<<"\t"<<blue<<std::endl;
+
+		//Change Color
+		ledBar[0][0] = (uint8_t)(blue1);
+		ledBar[0][1] = (uint8_t)(red1);
+		ledBar[0][2] = (uint8_t)(green1);
+		loadWS2803();
+
+		return true;
+	}
+	else if(msl::starts_with(request,"/color2?"))
+	{
+		//Change ?'s to spaces
+		for(unsigned int ii=0;ii<request.size();++ii)
+			if(request[ii]=='?')
+				request[ii]=' ';
+
+		//Variables for Parsing
+		std::istringstream istr(request);
+		std::string header;
+		int red2=0;
+		int green2=0;
+		int blue2=0;
+
+		//Get Header
+		istr>>header;
+
+		//Get Color
+		istr>>red2;
+		istr>>green2;
+		istr>>blue2;
+
+		//Print Color
+		//std::cout<<"change color\t"<<red<<"\t"<<green<<"\t"<<blue<<std::endl;
+
+		//Change Color
+		ledBar[0][3] = (uint8_t)(blue2);
+		ledBar[0][4] = (uint8_t)(red2);
+		ledBar[0][5] = (uint8_t)(green2);
+		loadWS2803();
+
+		return true;
+	}
+	else if(msl::starts_with(request,"/color3?"))
+	{
+		//Change ?'s to spaces
+		for(unsigned int ii=0;ii<request.size();++ii)
+			if(request[ii]=='?')
+				request[ii]=' ';
+
+		//Variables for Parsing
+		std::istringstream istr(request);
+		std::string header;
+		int red3=0;
+		int green3=0;
+		int blue3=0;
+
+		//Get Header
+		istr>>header;
+
+		//Get Color
+		istr>>red3;
+		istr>>green3;
+		istr>>blue3;
+
+		//Print Color
+		//std::cout<<"change color\t"<<red<<"\t"<<green<<"\t"<<blue<<std::endl;
+
+		//Change Color
+		ledBar[0][6] = (uint8_t)(blue3);
+		ledBar[0][7] = (uint8_t)(red3);
+		ledBar[0][8] = (uint8_t)(green3);
+		loadWS2803();
+
+		return true;
+	}
+	else if(msl::starts_with(request,"/color4?"))
+	{
+		//Change ?'s to spaces
+		for(unsigned int ii=0;ii<request.size();++ii)
+			if(request[ii]=='?')
+				request[ii]=' ';
+
+		//Variables for Parsing
+		std::istringstream istr(request);
+		std::string header;
+		int red4=0;
+		int green4=0;
+		int blue4=0;
+
+		//Get Header
+		istr>>header;
+
+		//Get Color
+		istr>>red4;
+		istr>>green4;
+		istr>>blue4;
+
+		//Print Color
+		//std::cout<<"change color\t"<<red<<"\t"<<green<<"\t"<<blue<<std::endl;
+
+		//Change Color
+		ledBar[0][9] = (uint8_t)(blue4);
+		ledBar[0][10] = (uint8_t)(red4);
+		ledBar[0][11] = (uint8_t)(green4);
+		loadWS2803();
+
 		return true;
 	}
 
