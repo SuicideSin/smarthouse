1d89e41191c080a4f2b843e39b949b4ac36b4073
Mike Moss <mrmoss@alaska.edu>
2013-08-01 17:20:17 -0800

    Initial commit.

---
+++ main.cpp
@@ -0,0 +1,129 @@
+//IO Stream Header
+#include <iostream>
+
+//JSON Header
+#include "msl/json.hpp"
+
+//Serial Sync Header
+#include "SerialSync.hpp"
+
+//Socket Header
+#include "msl/socket.hpp"
+
+//Socket Utility Header
+#include "msl/socket_util.hpp"
+
+//String Stream Header
+#include <sstream>
+
+//String Utility Header
+#include "msl/string_util.hpp"
+
+//Web Server Header
+#include "msl/webserver.hpp"
+
+//Our Service Client Function Declaration
+bool service_client(msl::socket& client,const std::string& message);
+
+//Global Serial Sync Object
+SerialSync ss("/dev/ttyUSB0",9600);
+
+//Main
+int main(int argc,char* argv[])
+{
+	//Server Variables
+	std::string server_port="8080";
+	std::string server_serial="/dev/ttyUSB0";
+	unsigned int server_baud=9600;
+	bool server_passed=true;
+
+	//Get Command Line Port
+	if(argc>1)
+		server_port=argv[1];
+
+	//Get Command Line Serial Port
+	if(argc>2)
+		server_serial=argv[2];
+
+	//Get Command Line Serial Baud
+	if(argc>3)
+		server_baud=msl::to_int(argv[3]);
+
+	//Create Server
+	msl::webserver server("0.0.0.0:"+server_port,service_client);
+	server.setup();
+
+	//Check Server
+	if(server.good())
+	{
+		std::cout<<"Socket "<<server_port<<" =)"<<std::endl;
+	}
+	else
+	{
+		std::cout<<"Socket "<<server_port<<" =("<<std::endl;
+		server_passed=false;
+	}
+
+	//Check Serial
+	try
+	{
+		ss=SerialSync(server_serial,server_baud);
+		ss.setup();
+		std::cout<<"Serial "<<server_serial<<" "<<server_baud<<" =)"<<std::endl;
+	}
+	catch(...)
+	{
+		std::cout<<"Serial "<<server_serial<<" "<<server_baud<<" =("<<std::endl;
+		server_passed=false;
+	}
+
+	//Be a server...forever...
+	while(server_passed)
+	{
+		//Update Server
+		server.update();
+
+		//Update Serial
+		ss.loop();
+
+		//Give OS a Break
+		usleep(0);
+	}
+
+	//Call Me Plz T_T
+	return 0;
+}
+
+//Our Service Client Function Definition
+bool service_client(msl::socket& client,const std::string& message)
+{
+	//Create Parser
+	std::istringstream istr(message);
+
+	//Parse the Request
+	std::string request;
+	istr>>request;
+	istr>>request;
+
+	//Check For a Custom Request
+	if(request=="/temperatures")
+	{
+		//Debug
+		std::cout<<"Temp request!\t"<<ss.get(0)<<std::endl;
+
+		msl::json temperatures;
+		temperatures.set("0",ss.get(0));
+		temperatures.set("1",ss.get(1));
+		temperatures.set("2",ss.get(2));
+		temperatures.set("3",ss.get(3));
+
+		//Send Custom Message
+		client<<msl::http_pack_string(temperatures.str(),"text/plain");
+
+		//Return True (We serviced the client)
+		return true;
+	}
+
+	//Default Return False (We did not service the client)
+	return false;
+}
